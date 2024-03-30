80b757811f8e155a2d4076f5b8d43930867e395f
gokulnpc <gokulnpc@gmail.com>
2024-03-30 16:17:41 +0530

    plant

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ options = st.sidebar.selectbox('Select a page:',
                            ['Prediction', 'Code', 'About'])
 
 if options == 'Prediction': # Prediction page
-    st.title('Handwritten Digit Classification with Neural Networks')
+    st.title('Plant Disease Prediction')
 
     image = st.file_uploader('Upload an image:', type=['jpg', 'jpeg', 'png'])
     if image is not None:
         st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    if st.button('Predict'):
+    if st.button('Classify Image'):
         st.write(f'The predicted digit is:')
              
 elif options == 'Code':
